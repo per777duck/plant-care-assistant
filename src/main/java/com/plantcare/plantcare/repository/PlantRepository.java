b68343734479504e09fb3c42e2159b15f02692e0
ivachqq <ivanpes530@gmail.com>
2026-07-03 17:24:58 +0300

    made all plant

--- src/main/java/com/plantcare/plantcare/repository/PlantRepository.java
+++ src/main/java/com/plantcare/plantcare/repository/PlantRepository.java
@@ -4,6 +4,9 @@ import com.plantcare.plantcare.entity.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlantRepository extends JpaRepository<Plant, Long> {
+    List<Plant> findByNameContainingIgnoreCase(String name);
 }
